bfea18a969805a9b1a9f1f637422df8edffc08f9
Jose Filho <alljosefilho@gmail.com>
2024-04-12 17:35:52 -0300

    Mais um projeto de muitos que virão

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+const botao = document.querySelector(".btn-plataforma");
+
+const elementoPlataforma = document.querySelector(".btn-plataforma .plataformas")
+
+botao.addEventListener("click", () => {
+  const botaoEstaAberto = elementoPlataforma.classList.contains("ativo");
+ if(botaoEstaAberto) {
+      elementoPlataforma.classList.remove("ativo");
+     }else{
+      elementoPlataforma.classList.add("ativo")
+     }
+  
+
+});
+
